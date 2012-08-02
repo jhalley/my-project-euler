304fc5d4a815b198d48a799a9b42a3ca34750778
Jhalley Badua de Castro <joseph.halley@gmail.com>
2012-08-02 18:08:19 +0800

    PE 92
        - Made some changes that made it faster: 1m28.089s

--- 92.py
+++ 92.py
@@ -3,12 +3,14 @@ from itertools import permutations as p
 MEMOIZED = {} 
 MEMOIZED[1] = 1
 MEMOIZED[89] = 89
-END_NUM = {1:1, 89:1}
+END_NUM = {1:0, 89:0}
 UPPER_LIMIT = 10000000
 
 def pe92():
     for i in xrange(2, UPPER_LIMIT): # we start at 2 because 0 is meaningless and we already know the value for 1
         figure_end(i)
+    for i in xrange(1, UPPER_LIMIT):
+        END_NUM[MEMOIZED[i]] += 1
     print END_NUM
 
 def figure_end(x):
@@ -21,9 +23,9 @@ def figure_end(x):
         return MEMOIZED[x]
     else:
         final_num = figure_end(sum([int(i)**2 for i in str(x)]))
-        MEMOIZED[x] = final_num
-        if x < UPPER_LIMIT:
-            END_NUM[final_num] += 1
+        for k in set([int(''.join(j)) for j in p([i for i in str(x)])]):
+            MEMOIZED[k] = final_num
+
         return final_num
 
 pe92()
